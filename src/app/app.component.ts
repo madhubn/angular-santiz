94c2cced33c861a8963e31b8d48ada12fc252ba9
madhubn <madhubn.17@gmail.com>
2020-02-04 15:55:47 +0530

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, SecurityContext } from "@angular/core";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  name = "Angular";
+  html: string;
+  h_html: string;
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.html = '<svg onload="alert(1)"> blah </svg>';
+    this.h_html = sanitizer.sanitize(
+      SecurityContext.HTML,
+      '<svg onload="alert(2)"> blah </svg>'
+    );
+  }
+
+  onChange(data: any) {
+    console.log("data", data);
+    this.h_html = this.sanitizer.sanitize(SecurityContext.HTML, data);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { SafePipe } from "./safe.pipe";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+  declarations: [AppComponent, HelloComponent, SafePipe],
+  exports: [SafePipe],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
