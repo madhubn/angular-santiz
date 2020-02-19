132b4cb1805fcf537aa2b22822c6d26b8849491a
madhubn <madhubn.17@gmail.com>
2020-02-19 10:41:30 +0530

    1 file modified

--- src/app/linear-gauge/linear-gauge.component.ts
+++ src/app/linear-gauge/linear-gauge.component.ts
@@ -17,63 +17,68 @@ export class LinearGaugeComponent implements OnInit {
   min = 0;
   max = 220;
   majorTick: any;
-  constructor() {
-    this.initOptions();
-  }
+  constructor() {}
 
   ngOnInit() {
-    // const data = [];
-    // const max = this.max / 10;
-    // for (let i = this.min; i <= max; i++) {
-    //   data.push(i * 10);
-    // }
-    // this.majorTick = data;
+    const data = [];
+    const max = this.max / 10;
+    const loop = max/2;
+    for (let i = this.min; i <= loop; i++) {
+      data.push(i * 20);
+    }
+    this.majorTick = data;
     this.value$ = interval(2000).pipe(map(() => 20));
 
     console.log("this.majorTick", this.majorTick);
+    this.initOptions();
   }
 
   private initOptions() {
     this.scaleGaugeOptions = {
-      title: "Temperature ",
+      title: "Temperature (°C) ",
 
       width: "400",
       height: "150",
-      units: "Lbs",
+      // units: "Lbs",
       minValue: 0,
       maxValue: 220,
-      // majorTicks: "0,20,40,60,80,100,120,140,160,180,200,220",
-      minorTicks: "2",
-      strokeTicks: "true",
-      highLights: "true",
-      borders: "false",
+      majorTicks: this.majorTick,
+      minorTicks: "5",
+      // majorTicksInt: 100,
+      // strokeTicks: "true",
+      highlightsWidth:0,
+      highLights:"false",
+      exactTicks: "true",
+      borders: 0,
+      boxStroke: 0,
+      borderMiddleWidth:0,
+      borderInnerWidth:0,
+      borderShadowWidth:0,
+      colorValueBoxBackground:false,
       colorPlate: "black",
-      borderShadowWidth: 0,
       barBeginCircle: 0,
       barWidth: 10,
       tickSide: "left",
-      numberSide:"left",
+      numberSide: "left",
       needleSide: "left",
-      needleType:"arrow",
-      needleWidth:"20",
-      colorNeedle:"blue",
-      colorNeedleEnd:"rgba(255, 160, 122, .9)",
-      animationTarget:"plate",
+      needleType: "arrow",
+      needleWidth: "20",
+      colorNeedle: "blue",
+      colorNeedleEnd: "rgba(255, 160, 122, .9)",
       ticksWidth: 20,
-      fontNumbersSize:20,
-      ticksWidthMinor: 20,
-      ticksPadding:10,
-      colorMajorTicks:"#fff",
-      colorMinorTicks:"#fff",
-      colorStrokeTicks:"#fff",
-      colorTitle:"#fff",
-      colorNumbers:"#fff",
-      colorBar:"red",
-      colorBarProgress:"green",
-      
-      // colorBar: "red",
+      fontNumbersSize: 20,
+      ticksWidthMinor: 10,
+      ticksPadding:5,
+      colorMajorTicks: "#fff",
+      colorMinorTicks: "#fff",
+      colorStrokeTicks: "#fff",
+      colorTitle: "#fff",
+      colorNumbers: "#fff",
+      colorBar: "red",
+      colorBarProgress: "green",
+      animationTarget: "plate",
       animationRule: "linear",
-      animationDuration: "500"
+      animationDuration: "1500"
     };
   }
 }
