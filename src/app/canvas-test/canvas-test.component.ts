558da0f1a32074ded9edca5afa0024f6f4f254b4
madhubn <madhubn.17@gmail.com>
2020-12-24 16:42:10 +0530

    3 files modified

--- src/app/canvas-test/canvas-test.component.ts
+++ src/app/canvas-test/canvas-test.component.ts
@@ -6,45 +6,86 @@ import {
   ViewChild
 } from "@angular/core";
 import { Square } from "./square";
-
+import * as d3 from "d3";
+import * as d3Scale from "d3";
+import * as d3Shape from "d3";
+import * as d3Array from "d3";
+import * as d3Axis from "d3";
 @Component({
   selector: "app-canvas-test",
   templateUrl: "./canvas-test.component.html",
   styleUrls: ["./canvas-test.component.css"]
 })
 export class CanvasTestComponent implements OnInit {
-  @ViewChild("canvas", { static: true }) canvas: ElementRef<HTMLCanvasElement>;
-  ctx: CanvasRenderingContext2D;
-  requestId;
-  interval;
-  squares: Square[] = [];
+  public title = "Line Chart";
+  data: any[] = [
+    { date: new Date("2010-01-01"), value: 40 },
+    { date: new Date("2010-01-04"), value: 93 },
+    { date: new Date("2010-01-05"), value: 95 },
+    { date: new Date("2010-01-06"), value: 130 },
+    { date: new Date("2010-01-07"), value: 110 },
+    { date: new Date("2010-01-08"), value: 120 },
+    { date: new Date("2010-01-09"), value: 129 },
+    { date: new Date("2010-01-10"), value: 107 },
+    { date: new Date("2010-01-11"), value: 140 }
+  ];
 
-  constructor(private ngZone: NgZone) {}
+  private margin = { top: 20, right: 20, bottom: 30, left: 50 };
+  private width: number;
+  private height: number;
+  private x: any;
+  private y: any;
+  private svg: any;
+  private line: d3Shape.Line<[number, number]>; // this is line defination
 
-  ngOnInit() {
-    this.ctx = this.canvas.nativeElement.getContext("2d");
-    this.ctx.fillStyle = "red";
-    this.ngZone.runOutsideAngular(() => this.tick());
-    setInterval(() => {
-      this.tick();
-    }, 200);
+  constructor() {
+    // configure margins and width/height of the graph
+    this.width = 960 - this.margin.left - this.margin.right;
+    this.height = 500 - this.margin.top - this.margin.bottom;
+  }
+  public ngOnInit(): void {
+    this.buildSvg();
+    this.addXandYAxis();
+    this.drawLineAndPath();
   }
 
-  tick() {
-    this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
-    this.squares.forEach((square: Square) => {
-      square.moveRight();
-    });
-    this.requestId = requestAnimationFrame(() => this.tick);
+  private buildSvg() {
+    this.svg = d3
+      .select("svg")
+      .append("g")
+      .attr(
+        "transform",
+        "translate(" + this.margin.left + "," + this.margin.top + ")"
+      );
+  }
+  private addXandYAxis() {
+    // range of data configuring
+    this.x = d3Scale.scaleTime().range([0, this.width]);
+    this.y = d3Scale.scaleLinear().range([this.height, 0]);
+    this.x.domain(d3Array.extent(this.data, d => d.date));
+    this.y.domain(d3Array.extent(this.data, d => d.value));
+    // Configure the X Axis
+    this.svg
+      .append("g")
+      .attr("transform", "translate(0," + this.height + ")")
+      .call(d3Axis.axisBottom(this.x));
+    // Configure the Y Axis
+    this.svg
+      .append("g")
+      .attr("class", "axis axis--y")
+      .call(d3Axis.axisLeft(this.y));
   }
 
-  play() {
-    const square = new Square(this.ctx);
-    this.squares = this.squares.concat(square);
-  }
-
-  ngOnDestroy() {
-    clearInterval(this.interval);
-    cancelAnimationFrame(this.requestId);
+  private drawLineAndPath() {
+    this.line = d3Shape
+      .line()
+      .x((d: any) => this.x(d.date))
+      .y((d: any) => this.y(d.value));
+    // Configuring line path
+    this.svg
+      .append("path")
+      .datum(this.data)
+      .attr("class", "line")
+      .attr("d", this.line);
   }
 }
